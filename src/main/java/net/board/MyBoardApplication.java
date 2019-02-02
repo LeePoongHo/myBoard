f060ebb178bbd726811e390557ac003618c08ebf
LeePoongHo <dlvndghs@naver.com>
2019-02-02 11:44:56 +0900

    init

---
+++ src/main/java/net/board/MyBoardApplication.java
@@ -0,0 +1,14 @@
+package net.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBoardApplication.class, args);
+	}
+
+}
+
